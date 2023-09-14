79a2cb9606eaea0c3027d9daf03755b6aa3ef751
igutierrezzz78 <igutierrezzz78@gmail.com>
2023-09-14 08:17:54 +0200

    vacio 14/09/2023 8:17

---
+++ ExamenApi/Entidades/Persona.cs
@@ -0,0 +1,16 @@
+﻿namespace Entidades
+{
+    public class Persona
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+    
+        public string apellido { get; set; }
+
+        public string password { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public string? Telefono { get; set; }
+    }
+}
